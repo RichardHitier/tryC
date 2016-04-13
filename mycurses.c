52c1a001f2e3c36285cf0517558c189d62c1027d
Richard Hitier <richard.hitier@co-libri.org>
2016-04-13 14:39:34 +0200

    draw borders

--- mycurses.c
+++ mycurses.c
@@ -3,7 +3,7 @@
 #include <ncurses.h>
 
 
-void showWins(int i){
+void showWins(){
 
     WINDOW *winRight, *winLeft, *winLow;
 
@@ -11,19 +11,35 @@ void showWins(int i){
     const int tot_width=80;
     const int higher=2*max_channels/3;
 
+    int n, p;
+
     initscr();
 
     winLeft  = subwin( stdscr, higher, tot_width/2, 2, 0);
     winRight = subwin( stdscr, higher, tot_width/2, 2, tot_width/2);
-    winLow   = subwin( stdscr, max_channels/3,   tot_width, higher, 0);
+    winLow   = subwin( stdscr, max_channels/3,   tot_width, higher+2, 0);
 
-    while(i-->0){
-        mvwprintw( winLeft, 0, 0, "Hello winLeft\n");
-        mvwprintw( winRight, 0, 0, "Hello winRight\n");
-        mvwprintw( winLow, 0, 0, "Hello winLow\n");
+
+
+    while(1){
+        mvwprintw( winLeft, 1, 1, "Hello winLeft\n");
+        mvwprintw( winRight, 1, 1, "Hello winRight\n");
+        mvwprintw( winLow, 1, 1, "Hello winLow\n");
+        box(winLeft, ACS_VLINE, ACS_HLINE);
+        box(winRight, ACS_VLINE, ACS_HLINE);
+        box(winLow, ACS_VLINE, ACS_HLINE);
+
+        int yoff=2, xoff=1;
+
+        for( n=0; n<4; n++){
+            for( p=0; p<4; p++){
+                mvwprintw( winLeft, yoff+n, xoff+p, ".");
+            }
+        }
+
+
         refresh();
         sleep(1);
-
     }
 
     endwin();
@@ -31,6 +47,5 @@ void showWins(int i){
 }
 
 int main(int argc, char **argv){
-    int i = atoi(argv[1]);
-    showWins(i);
+    showWins();
 }
